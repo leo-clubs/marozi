4b783aa45f8da225f7fc97d7a0c35534953f2f43
Leif Gensert <leif.gensert@stylefruits.de>
2012-12-20 12:03:30 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Marozi::Application.config.secret_key_base = '72115e458890f184e360094068a825c61a4dddcc67fb7d9c51beee5043a3d24be723a6a16024c0044223242d4a1121f0b1f02c755b2d6731f2ca280b286e52e8'
