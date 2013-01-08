41338c7c0b086c98c161122396e0812385341d97
Leif Gensert <leif.gensert@stylefruits.de>
2013-01-08 13:05:11 +0100

    hide production secret token from version control

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -1,4 +1,4 @@
 # config/initializers/load_config.rb
 def app_config
-  @app_config ||= YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env]
+  @app_config ||= YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env] || {}
 end
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,12 +1,1 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-Marozi::Application.config.secret_key_base = '72115e458890f184e360094068a825c61a4dddcc67fb7d9c51beee5043a3d24be723a6a16024c0044223242d4a1121f0b1f02c755b2d6731f2ca280b286e52e8'
+Marozi::Application.config.secret_key_base = app_config['secret_token'] || '005d6c08efba5271aeac54fdd0504306fc536deb2e47eed4e9d886287b605c6279409d1871403063dce67ca592035339ce5eda3d4b51611ba14684eabce2d1b8'
