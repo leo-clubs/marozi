1787f3cb9a0c3d1d330d3ce195f1036f1e681a29
Leif Gensert <leif@gensert.de>
2013-01-11 19:13:21 +0100

    add helper for attribute row (needs testing)

--- app/helpers/members_helper.rb
+++ app/helpers/members_helper.rb
@@ -5,4 +5,13 @@ module MembersHelper
       form.radio_button(:gender, gender, checked: member.gender == gender)
     end.join("\n")
   end
+
+  def member_simple_attribute_tablerow field, name, type, caption
+    name_cell = content_tag(:td, {width: '15%'}) { name }
+    value_cell = content_tag(:td) { link_to(@member.send(field), '#', {id: field, :'data-field-type' => type, :'data-title' => caption})}
+
+    content_tag :tr do
+      name_cell.concat value_cell
+    end
+  end
 end
