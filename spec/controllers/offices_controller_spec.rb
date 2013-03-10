0be6e23f5ac11a24e967f2932969586a414b0fda
Leif Gensert <leif.gensert@leo-clubs.de>
2013-03-10 12:06:18 +0100

    add test for office controller

---
+++ spec/controllers/offices_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe OfficesController do
+  context 'protected methods' do
+    include_context 'protected controller'
+
+    describe '#update' do
+      it 'should change office correctly' do
+        office = create(:president_office)
+        new_member_id = office.member_id + 10
+        patch :update, id: office.leo_id, value: new_member_id
+        expect(office.reload.member_id).to eq new_member_id
+      end
+    end
+  end
+end
--- spec/factories/office.rb
+++ spec/factories/office.rb
@@ -1,6 +1,8 @@
 FactoryGirl.define do
   factory :office, class: Office do
     sequence(:leo_id) {|n| 1400 + n}
+    sequence(:member_id) {|n| 27000 + n}
+    sequence(:parent_id) {|n| 5000 + n}
     year '2012-2013'
 
     factory :president_office, class: Office do
