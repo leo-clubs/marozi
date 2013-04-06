ed787b5c82b54c15b5ea7dbb942f110bc420a1c9
Leif Gensert <leif.gensert@leo-clubs.de>
2013-04-06 12:25:27 +0200

    add key back to cookie store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Marozi::Application.config.session_store :cookie_store
+Marozi::Application.config.session_store :cookie_store, key: '_marozi_session'
